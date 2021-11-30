e7a5774d6feebaa0adc16ef9872b953535a17ee1
Afif Sohaili <afifnajib@gmail.com>
2021-11-30 15:18:27 +0800

    Add Puppeteer and Chrome Lambda layers

--- src/lambda.js
+++ src/lambda.js
@@ -1,7 +1,41 @@
+import chrome from "chrome-aws-lambda";
+
+const puppeteer = chrome.puppeteer;
+
 export async function handler(event) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: `Hello! You've requested to print the receipt at page ${event.queryStringParameters.url}`,
-  };
+  let browser
+  let response
+  try {
+    const { url } = event.queryStringParameters;
+
+    browser = await puppeteer.launch({
+      args: chrome.args,
+      executablePath: await chrome.executablePath,
+    });
+
+    const page = await browser.newPage();
+    const width = 874
+    const height = 1240
+
+    await page.setViewport({ width, height });
+
+    // Navigate to the url
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    // Take the screenshot
+    await page.pdf({path: 'receipt.pdf', width: width + "px", height: height + "px", printBackground: true});
+
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({message: 'Screenshot taken'})
+    };
+  } catch(err) {
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({message: `An error occured. ${err.message}`})
+    }
+  } finally {
+    await browser && browser.close()
+  }
+  return response
 }
--- stacks/MyStack.js
+++ stacks/MyStack.js
@@ -1,4 +1,7 @@
 import * as sst from "@serverless-stack/resources";
+import { LayerVersion } from "@aws-cdk/aws-lambda";
+
+const layerArn = "arn:aws:lambda:us-east-1:764866452798:layer:chrome-aws-lambda:25";
 
 export default class MyStack extends sst.Stack {
   constructor(scope, id, props) {
@@ -7,7 +10,17 @@ export default class MyStack extends sst.Stack {
     // Create a HTTP API
     const api = new sst.Api(this, "Api", {
       routes: {
-        "GET /downloads/receipt": "src/lambda.handler",
+        "GET /downloads/receipt": {
+          function: {
+            handler: "src/lambda.handler",
+            // Increase the timeout for generating screenshots
+            timeout: 15,
+            // Load Chrome in a Layer
+            layers: [LayerVersion.fromLayerVersionArn(this, "Layer", layerArn)],
+            // Exclude bundling it in the Lambda function
+            bundle: { externalModules: ["chrome-aws-lambda"] },
+          }
+        },
       },
     });
 
